7df2186b2917ce2247d7d3f858e7495062753088
Hao Guan <guanhao0930@nextdish.com>
2021-03-22 00:35:16 +0800

    modify file

---
+++ index.js
@@ -0,0 +1,22 @@
+const person = {
+  name: 'tom',
+  age: 17,
+  gender: 'male',
+  tags: [
+    'handsome',
+    'good looking',
+    'gorgeous'
+  ],
+  jobs: [
+    {
+      position: 'software engineer',
+      title: 'senior',
+      company: 'apple'
+    },
+    {
+      position: 'operation',
+      title: 'junior',
+      company: 'google'
+    }
+  ]
+}
